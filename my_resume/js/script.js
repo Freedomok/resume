887ae04262efe54a4932ae266bd1068ecdcbc59a
Freedomok <kniazkoff@gmail.com>
2023-03-01 16:27:51 +0200

    Update script.js

--- my_resume/js/script.js
+++ my_resume/js/script.js
@@ -1,1 +1,12 @@
-alert('Я JavaScript!');
+var password = "hyi";
+
+function checkPassword() {
+  var input = document.getElementById("password").value;
+  if (input == password) {
+    document.getElementById("content").style.display = "block";
+    document.getElementById("error-message").style.display = "none";
+  } else {
+    document.getElementById("content").style.display = "none";
+    document.getElementById("error-message").style.display = "block";
+  }
+}
